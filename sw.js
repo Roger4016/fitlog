43a9521b4e7b9885690d6fe2ae10ea32b668d845
Roger <a989298645@gmail.com>
2026-04-04 18:32:41 +0800

    Update service worker cache version to v16 and upgrade Chart.js script source to v4.5.1 for improved performance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'fitlog-v15';
+var CACHE = 'fitlog-v16';
 var UPDATE_TYPE = 'safe'; // 'silent' | 'safe' | 'data'
 var URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
