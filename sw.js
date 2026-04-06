990f638392c7e4176bb3262baf258a1f038bcc98
Roger <a989298645@gmail.com>
2026-04-06 16:27:50 +0800

    Add AI image processing parameters and update service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'fitlog-v16';
+var CACHE = 'fitlog-v17';
 var UPDATE_TYPE = 'safe'; // 'silent' | 'safe' | 'data'
 var URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
