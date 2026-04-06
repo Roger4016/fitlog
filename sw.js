25dac73ccb1bd98f2de7044d0ebd6c66ab7bb8bb
Roger <a989298645@gmail.com>
2026-04-06 17:17:49 +0800

    Refactor overlay positioning and update service worker cache version to v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'fitlog-v18';
+var CACHE = 'fitlog-v19';
 var UPDATE_TYPE = 'safe'; // 'silent' | 'safe' | 'data'
 var URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
